e388eb9deac503df7f647b9204582794a6852777
Devon Faria <devonfaria@gmail.com>
2022-03-29 00:38:11 -0400

    added inquirer questions, seeds.sql for testing, and constructor js files for classes

---
+++ assets/js/index.js
@@ -0,0 +1,136 @@
+// Linking required modules and dependencies
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Constructor functions for staff
+const Employee = require('../lib/employee');
+const Department = require('../lib/department');
+const Role = require('../lib/role');
+
+// Empty arrays to collect entered staff member information
+let employees = [];
+let cards = '';
+
+// Questions for terminal prompts, split by employee type
+const questionsDepartment = [
+  {
+    type: 'input',
+    message: "What is the name of the department?",
+    name: 'name',
+  },
+];
+
+const questionsRole = [
+  {
+    type: 'input',
+    message: "What is the name of the role",
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: "What is the salary of the role?",
+    name: 'salary',
+  },
+  {
+    type: 'rawlist',
+    message: "What department does the role belong to?",
+    name: 'department',
+    choices: [
+      {name: 'Executive Management', value: 'exec'},
+      {name: 'Accounting', value: 'acc'},
+      {name: 'Web Development', value: 'web-dev'},
+      {name: 'Data Science', value: 'data-sci'},
+    ]
+  },
+];
+
+const questionsEmployee = [
+  {
+    type: 'input',
+    message: "What is the employee's first name?",
+    name: 'first',
+  },
+  {
+    type: 'input',
+    message: "What is the employee's last name?",
+    name: 'last',
+  },
+  {
+    type: 'list',
+    message: "What is the employee's role?",
+    name: 'role',
+    choices: [
+      {name: 'CEO', value: 'ceo'},
+      {name: 'CFO', value: 'cfo'},
+      {name: 'Accountant', value: 'acc'},
+      {name: 'Web Developer', value: 'web-dev'},
+    ]
+  },
+  {
+    type: 'input',
+    message: "Who is the employee's manager?",
+    name: 'manager',
+    choices: [
+      {name: 'Devon Faria', value: 'df'},
+      {name: 'Deb Sparr', value: 'ds'},
+      {name: 'Rebecca Hill', value: 'rh'},
+      {name: 'Greg Hyde', value: 'gh'},
+    ]
+  },
+];
+
+const updateEmployee = [
+  {
+    type: 'input',
+    message: "Who is the employee's manager?",
+    name: 'manager',
+    choices: [
+      {name: 'Devon Faria', value: 'df'},
+      {name: 'Deb Sparr', value: 'ds'},
+      {name: 'Rebecca Hill', value: 'rh'},
+      {name: 'Greg Hyde', value: 'gh'},
+    ]
+  },
+];
+
+
+// Asks if you would like to add another employee
+const questionsNew = [
+  {
+  type: 'rawlist',
+  message: 'Would you like to do?',
+  name: 'type',
+  choices: [
+    {name: 'View All Employees', value: 'view-emp'},
+    {name: 'Add Employee', value: 'add-emp'},
+    {name: 'Update Employee Role', value: 'upd-emp'},
+    {name: 'View All Roles', value: 'view-role'},
+    {name: 'Add Role', value: 'add-role'},
+    {name: 'View All Departments', value: 'view-dept'},
+    {name: 'Add Department', value: 'add-dept'},
+    {name: 'Quit', value: 'quit'},
+  ]
+  },
+];
+
+// Initial questions prompt
+function promptInit() {
+  inquirer
+  .prompt(questionsNew)
+    .then ((data) => {
+      switch (data.value) {
+        case 'view-emp': return;
+        case 'add-emp': return;
+        case 'upd-emp': return;
+        case 'view-role': return;
+        case 'add-role': return;
+        case 'view-dept': return;
+        case 'add-dept': return;
+        case 'quit': return;
+      };
+    })
+    .catch((err) => {console.log(err)});
+};
+
+// Initiates program
+promptInit();
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+USE emptracker_db;
+
+INSERT INTO department (name)
+VALUES ("Web Development"),
+       ("Data Science"),
+       ("Executive Management"),
+       ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 200000, 3),
+       ("CFO", 180000, 3),
+       ("Web Developer", 120000, 1),
+       ("Intern", 50000, 1),
+       ("Accountant", 150000, 4),
+       ("Accounts Manager", 180000, 4),
+       ("MySQL Engineer", 120000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Devon", "Faria", 1),
+       ("Deb", "Sparr", 2),
+       ("Rebecca", "Hill", 6),
+       ("Heather", "Barnes", 5),
+       ("Michael", "Pearce", 7),
+       ("Johnny", "Pearce", 7),
+       ("Danielle", "Alfieri", 3),
+       ("Chris", "Spagnesi", 4),
+       ("Greg", "Hyde", 3),
+       ("Heather", "Smith", 4),
